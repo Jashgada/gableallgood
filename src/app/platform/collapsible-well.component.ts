854d22c5f08db185891788f80e26229c20466e4b
jashgada <jashgada93@gmail.com>
2021-02-06 22:24:47 -0500

    Added Oswald font

--- src/app/platform/collapsible-well.component.ts
+++ src/app/platform/collapsible-well.component.ts
@@ -2,7 +2,19 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'collapsible-well',
-    templateUrl: './collapsible-well.component.html' 
+    templateUrl: './collapsible-well.component.html',
+    styles:[
+    `p{
+        font-family: 'Oswald', sans-serif;
+        font-weight: 300;
+        font-size: 11pt;
+    }
+    
+    H3{
+        font-family: 'Oswald', sans-serif;
+        font-weight: 600;
+        font-size: 28pt;
+    }`]
 })
 export class CollapsibleWellComponent implements OnInit{
     @Input() title: string;
